2bb372f1919e4f6d2276d025d4fcaddb6e33946f
stimorol95 <artem2695post@yandex.ru>
2022-05-07 22:42:28 +0300

    react_branch_5

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 import s from "./Navbar.module.css";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
-            <div className={s.item}><a>Profile</a> </div>
-            <div className={s.item}><a>Messages</a></div>
-            <div className={s.item}><a>News</a></div>
-            <div className={s.item}><a>Music</a></div>
-            <div className={s.item}><a>Settings</a></div>
+            <div className={s.item}><NavLink to="/profile">Profile</NavLink> </div>
+            <div className={s.item}><NavLink to="/dialogs">Messages</NavLink></div>
+            <div className={s.item}><NavLink to="/news">News</NavLink></div>
+            <div className={s.item}><NavLink to="/music">Music</NavLink></div>
+            <div className={s.item}><NavLink to="/settings">Settings</NavLink></div>
         </nav >);
 }
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,7 +4,7 @@ import s from "./Profile.module.css";
 
 const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <img src={require("../../images/japan.jpg")} />
             <div>
                 avatar + description
